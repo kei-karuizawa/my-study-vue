0a47b546e27be3fa5c30444add6eb6fb0c8cba59
Kiriha <grass.ichika@gmail.com>
2024-04-07 13:16:23 +0800

    临时保存

---
+++ src/todolist/views/Home.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from 'vue'
+import NavHeader from '@/todolist/components/navHeader/NavHeader.vue'
+import NavMain from '@/todolist/components/navMain/NavMain.vue'
+import NavFooter from '@/todolist/components/navFooter/NavFooter.vue'
+// export default defineComponent({
+//   name: 'Home',
+//   components: {
+//     NavHeader,
+//     NavMain,
+//     NavFooter
+//   },
+//   setup() {
+//
+//   }
+// })
+let num = ref(10)
+let name = ref('jack')
+let arr = ref(['1', '2', '3'])
+</script>
+
+<template>
+<!--  <div>-->
+<!--    <NavHeader></NavHeader>-->
+<!--    <NavMain></NavMain>-->
+<!--    <NavFooter></NavFooter>-->
+<!--  </div>-->
+  <div>
+    {{num}}
+  </div>
+  <div>
+    {{name}}
+  </div>
+  <div>
+    {{arr}}
+  </div>
+  <div>
+    {{arr[0]}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
