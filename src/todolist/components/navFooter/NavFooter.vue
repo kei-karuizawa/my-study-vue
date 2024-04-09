295cfcb8a22bebc27ff02564a16b095b5c1c3dba
月见坂桐叶 <git_kiriha@163.com>
2024-04-09 14:49:22 +0800

    feature：部分组件改用组合式。

--- src/todolist/components/navFooter/NavFooter.vue
+++ src/todolist/components/navFooter/NavFooter.vue
@@ -1,16 +1,7 @@
-<script>
-import { defineComponent, ref } from 'vue'
-export default defineComponent({
-  name: 'NavFooter',
-  setup() {
-    let isComplete = ref(1)
-    let all = ref(3)
-    return {
-      isComplete,
-      all
-    }
-  }
-})
+<script setup>
+import { ref } from 'vue'
+let isComplete = ref(1)
+let all = ref(3)
 </script>
 
 <template>
--- src/todolist/views/Home.vue
+++ src/todolist/views/Home.vue
@@ -3,20 +3,6 @@
 import NavHeader from '@/todolist/components/navHeader/NavHeader.vue'
 import NavMain from '@/todolist/components/navMain/NavMain.vue'
 import NavFooter from '@/todolist/components/navFooter/NavFooter.vue'
-// export default defineComponent({
-//   name: 'Home',
-//   components: {
-//     NavHeader,
-//     NavMain,
-//     NavFooter
-//   },
-//   setup() {
-//
-//   }
-// })
-// let num = ref(10)
-// let name = ref('jack')
-// let arr = ref(['1', '2', '3'])
 </script>
 
 <template>
@@ -25,18 +11,6 @@ import NavFooter from '@/todolist/components/navFooter/NavFooter.vue'
     <NavMain></NavMain>
     <NavFooter></NavFooter>
   </div>
-<!--  <div>-->
-<!--    {{num}}-->
-<!--  </div>-->
-<!--  <div>-->
-<!--    {{name}}-->
-<!--  </div>-->
-<!--  <div>-->
-<!--    {{arr}}-->
-<!--  </div>-->
-<!--  <div>-->
-<!--    {{arr[0]}}-->
-<!--  </div>-->
 </template>
 
 <style scoped>
