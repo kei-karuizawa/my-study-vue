816723b94f963e5df999c185a19a7ff927049760
月见坂桐叶 <git_kiriha@163.com>
2024-04-09 16:44:56 +0800

    feature：vuex 安装

--- src/todolist/views/Home.vue
+++ src/todolist/views/Home.vue
@@ -1,8 +1,14 @@
 <script setup>
-//import {ref} from 'vue'
+import { useStore } from 'vuex'
+import { computed } from 'vue'
 import NavHeader from '@/todolist/components/navHeader/NavHeader.vue'
 import NavMain from '@/todolist/components/navMain/NavMain.vue'
 import NavFooter from '@/todolist/components/navFooter/NavFooter.vue'
+
+let store = useStore()
+let list = computed(() => {
+  return store.state.list
+})
 </script>
 
 <template>
